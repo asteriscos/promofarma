cf3311148482c2962ebda9bac9f9bc33d5e96625
Federico Rodriguez <solosilencio@gmail.com>
2023-02-20 19:06:31 +0100

    Fix click handler

--- src/hocs/with-cart-button.tsx
+++ src/hocs/with-cart-button.tsx
@@ -1,17 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { addToCart } from '../store';
+import { addToCart, removeFromCart } from '../store';
 import { Icon } from '../components/icon/icon';
 
 export default (WrappedComponent: FC<any>) => {
 
   const Component = ({ isInCart, product, ...props }: any) => {
     const dispatch = useDispatch();
-
+    const handleClick = useCallback(() => {
+      isInCart ? dispatch(removeFromCart(product)) : dispatch(addToCart(product))
+    }, [isInCart])
     return <div className='with-button-wrapper'>
       <WrappedComponent isInCart={isInCart} product={product} {...props} />
-      <button title="Agregar al carrito" className='icon-button' disabled={isInCart} onClick={() => dispatch(addToCart(product))}>
-        <Icon color='#fff'/>
+      <button title="Agregar al carrito" className='icon-button' data-incart={isInCart} onClick={handleClick}>
+        <Icon color='#fff' />
       </button>
     </div>
   }
